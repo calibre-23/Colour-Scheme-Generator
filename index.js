5759fe07b51aa52d77a8d721d88baaf3fb385d18
Frankiegit config --global user.name Frankiegit config --global user.name Frankie <Frankiebrown996@gmail.com>
2025-03-31 23:11:28 +0100

    fuck

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-fetch("https://www.thecolorapi.com/id?hex=0047AB")
-    .then(res=> res.json())
-    .then(data=> console.log(data))
+
+
+
+function getColors() {
+    fetch("https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6")      
+     .then(response => response.json())
+        .then(data => {
+            const colors = data.colors.map(color => color.hex.value);
+            const colorDivs = colors.map(color => `<div class="color" style="background-color: ${color}">${color}</div>`).join('');
+            document.getElementById('colors').innerHTML = colorDivs;
+
+}
